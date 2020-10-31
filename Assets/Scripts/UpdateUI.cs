6fe4ff6730de85b7ede14bd11a5715b2c7a67c88
Sam Williams <c3207769@uon.edu.au>
2020-10-31 19:15:54 +1100

    add code to handle update of 'lives left'

--- Assets/Scripts/UpdateUI.cs
+++ Assets/Scripts/UpdateUI.cs
@@ -5,17 +5,26 @@
 using UnityEngine;
 using UnityEngine.UI;
 
+//Script used to update the UI elements at the top of the screen (ie. pizzas collected, lives left)
 public class UpdateUI : MonoBehaviour
-{    
+{
     //class variables
     private int pizzaCount = 0;
+    private int playerLives = 0;
     private Text pizzaText;
+    private Text livesText;
     void Update()
     {
+        //'pizza count' UI elements
         pizzaCount = gameObject.GetComponentInParent<PlayerMovement>().PizzaCount;
         pizzaText = gameObject.transform.Find("PizzaText").gameObject.GetComponentInChildren<Text>();
-        pizzaText.text = "Pizza Count: "+ pizzaCount;    
+        pizzaText.text = "Pizza Count: " + pizzaCount + "/4";
+
+        //'lives remaining' UI elements  
+        playerLives = gameObject.GetComponentInParent<PlayerMovement>().PlayerLives;
+        livesText = gameObject.transform.Find("LivesText").gameObject.GetComponentInChildren<Text>();
+        livesText.text = "Lives Remaining: " + playerLives;
     }
-    
-    
+
+
 }
