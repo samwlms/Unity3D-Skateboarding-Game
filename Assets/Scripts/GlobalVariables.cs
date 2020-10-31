c34879d58f9bae4978fa79f336478f8bda7cf0b9
Sam Williams <c3207769@uon.edu.au>
2020-10-31 18:13:57 +1100

    add global variables file to hold game vars

---
+++ Assets/Scripts/GlobalVariables.cs
@@ -0,0 +1,12 @@
+﻿//written by: Sam williams 
+//email: swilliams9uon.edu.au
+//project: Assignment 3 - inft3960
+
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GlobaVariables : MonoBehaviour
+{
+   
+    
+}
