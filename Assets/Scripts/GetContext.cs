6456f85551402898e3741ab7cfc36cc8c368e25a
Sam Williams <c3207769@uon.edu.au>
2020-10-31 22:11:06 +1100

    added script to map global context to scene

---
+++ Assets/Scripts/GetContext.cs
@@ -0,0 +1,21 @@
+﻿//written by: Sam williams 
+//email: swilliams9uon.edu.au
+//project: Assignment 3 - inft3960
+
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+using UnityEngine.UI;
+
+//class used to figure out the next level based off the GlobalVars values
+public class GetContext : MonoBehaviour
+{
+    void Start()
+    {
+        int currentLevel = FindObjectOfType<GlobalVariables>().GetComponent<GlobalVariables>().CurrentLevel;
+        Debug.Log("The 'getContext' script has initialised");
+        Debug.Log("The current level stored is: "+currentLevel);
+
+    }
+
+}
