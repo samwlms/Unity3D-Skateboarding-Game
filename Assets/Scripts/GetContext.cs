1d1dc2ca04ad3a90d467f1c664f1e5035dd7c8e8
Sam Williams <c3207769@uon.edu.au>
2020-10-31 22:33:06 +1100

    add buttonscript arg | reformatting

--- Assets/Scripts/GetContext.cs
+++ Assets/Scripts/GetContext.cs
@@ -10,11 +10,15 @@ using UnityEngine.UI;
 //class used to figure out the next level based off the GlobalVars values
 public class GetContext : MonoBehaviour
 {
+    public ButtonScript localInstance;
     void Start()
     {
-        int currentLevel = FindObjectOfType<GlobalVariables>().GetComponent<GlobalVariables>().CurrentLevel;
+        GlobalVariables globalVars = FindObjectOfType<GlobalVariables>();
+        globalVars.CurrentLevel++;
+        int currentLevel = globalVars.CurrentLevel;
         Debug.Log("The 'getContext' script has initialised");
         Debug.Log("The current level stored is: "+currentLevel);
+        localInstance.SceneIndexToLoad = currentLevel;
 
     }
 
