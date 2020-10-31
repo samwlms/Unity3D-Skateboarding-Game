fecb9910c4711f4d9f5b50e9800c4be77b19543f
Sam Williams <c3207769@uon.edu.au>
2020-10-31 11:40:47 +1100

    add commenting for clarity

--- Assets/Scripts/VehincleController.cs
+++ Assets/Scripts/VehincleController.cs
@@ -2,24 +2,27 @@
 //email: swilliams9uon.edu.au
 //project: Assignment 2 - inft3960
 
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class VehincleController : MonoBehaviour
 {
+    //set the speed the vehicle will travel
     public float Speed = 0.005f;
 
     void Update()
     {
-        gameObject.transform.position += new Vector3(-Speed,0f,0f);
+        //Move the vehicle along a set X-axis path
+        gameObject.transform.position += new Vector3(-Speed* Time.deltaTime,0f,0f); //delatTime normalises speed across systems
     }
     
     void OnCollisionEnter2D(Collision2D col)
     {
+        //if the vehicle collides with the deadzone...
         if(col.transform.CompareTag("DeadZone"))
         {
+            //inverse the speed
             Speed = -Speed;
+            //flip vehicle horizontally
             Vector3 theScale = transform.localScale;
 		    theScale.x *= -1;
 		    transform.localScale = theScale;
