4a953eeb61dad136493872a7936ea5e39dde42ad
Sam Williams <c3207769@uon.edu.au>
2020-10-31 17:23:54 +1100

    commenting

--- Assets/Scripts/ButtonScript.cs
+++ Assets/Scripts/ButtonScript.cs
@@ -1,14 +1,16 @@
 ﻿//written by: Sam williams 
 //email: swilliams9uon.edu.au
-//project: Assignment 2 - inft3960
+//project: Assignment 3 - inft3960
 
 using UnityEngine;
 using UnityEngine.SceneManagement;
 public class ButtonScript : MonoBehaviour
 {    
+    //set the index (maps to build settings) of the scene to load by the button
     public int SceneIndexToLoad;
     void OnMouseDown() 
     {
+        //load the user specified scene on button click
         SceneManager.LoadScene(SceneIndexToLoad);    
     }
 }
