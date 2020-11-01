768fb232b9f32a975105df7f31bbbce8f558d33c
Sam Williams <c3207769@uon.edu.au>
2020-11-01 14:31:45 +1100

    add declaration of global variables file

--- Assets/Scripts/KillPlayer.cs
+++ Assets/Scripts/KillPlayer.cs
@@ -7,16 +7,25 @@ using UnityEngine;
 public class KillPlayer : MonoBehaviour
 {
     //set the spawn point to return the player to
-    [SerializeField]Transform SpawnPoint;
+    [SerializeField] Transform SpawnPoint;
+
+    private GlobalVariables globalVars;
+    void Start()
+    {
+        globalVars = FindObjectOfType<GlobalVariables>();
+    }
+
 
     void OnCollisionEnter2D(Collision2D col)
     {
         //if player collides with 'killplayer' gameobject..
-        if(col.transform.CompareTag("Player"))
+        if (col.transform.CompareTag("Player"))
         {
             //move the payer back to the spawn point
             col.transform.position = SpawnPoint.position;
+            //add a death to the global variables counter in the persistent gameobject
+            globalVars.deaths++;
         }
     }
-    
+
 }
