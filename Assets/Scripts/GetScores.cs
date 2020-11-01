601d4ade26c8ecfe4832dc39ab3875c02557b1fb
Sam Williams <c3207769@uon.edu.au>
2020-11-01 14:54:45 +1100

    script used to grab global vars and display at end

---
+++ Assets/Scripts/GetScores.cs
@@ -0,0 +1,24 @@
+﻿//written by: Sam williams 
+//email: swilliams9uon.edu.au
+//project: Assignment 3 - inft3960
+
+using UnityEngine;
+using UnityEngine.UI;
+
+//Script used to calculate thefinal scores at the end of the level
+public class GetScores : MonoBehaviour
+{
+    private GlobalVariables globalVars;
+    private Text pizzaText;
+    private Text deathText;
+    void Start()
+    {
+        globalVars = FindObjectOfType<GlobalVariables>();
+
+        pizzaText = gameObject.transform.Find("pizzas").gameObject.GetComponentInChildren<Text>();
+        pizzaText.text = globalVars.pizzas.ToString()+"/12";
+
+        deathText = gameObject.transform.Find("deaths").gameObject.GetComponentInChildren<Text>();
+        deathText.text = globalVars.deaths.ToString();
+    }
+}
