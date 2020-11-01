7035736427341afb27bf3b7d3c711c9e67bab810
Sam Williams <c3207769@uon.edu.au>
2020-11-01 14:55:23 +1100

    remove uneeded stuff

--- Assets/Scripts/UpdateUI.cs
+++ Assets/Scripts/UpdateUI.cs
@@ -10,20 +10,13 @@ public class UpdateUI : MonoBehaviour
 {
     //class variables
     private int pizzaCount = 0;
-    private int playerLives = 0;
     private Text pizzaText;
-    private Text livesText;
     void Update()
     {
         //'pizza count' UI elements
         pizzaCount = gameObject.GetComponentInParent<PlayerMovement>().PizzaCount;
         pizzaText = gameObject.transform.Find("PizzaText").gameObject.GetComponentInChildren<Text>();
         pizzaText.text = "Pizza Count: " + pizzaCount + "/4";
-
-        //'lives remaining' UI elements  
-        playerLives = gameObject.GetComponentInParent<PlayerMovement>().PlayerLives;
-        livesText = gameObject.transform.Find("LivesText").gameObject.GetComponentInChildren<Text>();
-        livesText.text = "Lives Remaining: " + playerLives;
     }
 
 
